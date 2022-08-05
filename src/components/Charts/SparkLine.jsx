4b51c9b2e1d3a84bc483abbaa2775848ba42dace
WizardIsHere <mshushant235@gmail.com>
2022-08-05 17:50:57 +0530

    🚧  Header  & Sparkline

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
     return (
         <SparklineComponent
