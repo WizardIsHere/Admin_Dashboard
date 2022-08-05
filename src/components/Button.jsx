989efee91cf1bec0285b662f4d7cf5b9d9fdc935
WizardIsHere <mshushant235@gmail.com>
2022-08-05 17:28:14 +0530

    💄 Homepage Charts Addded

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react'; 
+// import { useStateContext } from '../contexts/ContextProvider';
 
-const Button = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+//   const { setIsClicked, initialState } = useStateContext();
 
-export default Button
+  return (
+    <button
+      type="button"
+    //   onClick={() => setIsClicked(initialState)}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 hover:drop-shadow-xl `}
+    >
+     {text}
+    </button>
+  );
+};
+
+export default Button;
